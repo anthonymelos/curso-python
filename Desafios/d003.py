a9e527339f4feceeb2f273f5fe6950af7b8fedf6
Anthony de Melo <anthonymelo377@gmail.com>
2025-01-20 22:10:24 -0300

    Alterações python

--- Desafios/d003.py
+++ Desafios/d003.py
@@ -1,9 +1,9 @@
-#Crie um script que leia ois numeros e
-#realize a soma entre eles
+#Crie um script que leia dois numeros e realize a soma entre eles
 
 print('Desafio 3')
 
-num1 = input('Primeiro número: ')
-num2 = input('Segundo número: ')
+num1 = int(input('Primeiro número: '))
+num2 = int(input('Segundo número: '))
 soma = num1 + num2
-print('Soma: ', int(soma))
+print ('Soma: ', soma)
+
