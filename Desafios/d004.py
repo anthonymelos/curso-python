8dbacb9d553ba04ab985cbe4a17ce2832db4d3eb
Anthony de Melo <anthonymelo377@gmail.com>
2025-01-22 23:19:50 -0300

    Esudos de python
    operadores e strings

--- Desafios/d004.py
+++ Desafios/d004.py
@@ -7,7 +7,8 @@ dado = input('Entre com a informação: ')
 print(type(dado),'\n\n')
 
 print(f'{dado} é alfabético?', dado.isalpha())
-print(f'{dado} é númerico?', dado.isnumeric())
+print(f'{dado} é um número?', dado.isnumeric())
 print(f'{dado} está em caixa alta?', dado.isupper())
 print(f'{dado} está em caixa baixa?', dado.islower())
 print(f'{dado} é alfanumerico?', dado.isalnum())
+
