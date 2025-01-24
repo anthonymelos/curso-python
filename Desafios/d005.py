1c38c869cc97e0dab54910cd7ed34b144bf62b7d
Anthony de Melo <anthonymelo377@gmail.com>
2025-01-23 22:09:20 -0300

    Exercicios de Python

---
+++ Desafios/d005.py
@@ -0,0 +1,6 @@
+#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+print('Desafio 005')
+num = int(input('Digite um número: '))
+print(f'O antecessor é: {num - 1}')
+print(f'O sucessor é: {num + 1}' )
---
+++ Desafios/d007.py
@@ -0,0 +1,6 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média
+
+print('Desafio 7')
+num1 = float(input('Informe a primeira nota: '))
+num2 = float(input('Informe a segunda nota: '))
+print(f'A média é {(num1 + num2)/2}')
