4f721acbafd66c4921fb3ec517bf5710154defc9
Anthony de Melo <anthonymelo377@gmail.com>
2025-01-28 22:19:13 -0300

    Exercicios

--- Desafios/d021.py
+++ Desafios/d021.py
@@ -1,2 +1,16 @@
 #O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
 
+import random
+
+print('Desafio 21')
+
+alunos = []
+
+alunos.append(input('Informe o nome do 1° aluno: '))
+alunos.append(input('Informe o nome do 2° aluno: '))
+alunos.append(input('Informe o nome do 3° aluno: '))
+alunos.append(input('Informe o nome do 4° aluno: '))
+
+random.shuffle(alunos)
+
+print(f'A ordem de apresentação será {alunos}')
---
+++ Desafios/d022.py
@@ -0,0 +1,3 @@
+#Faça um programa em Python que abra e reproduza o áudio de um arquivo mp3
+
+print('Desafio 22')
