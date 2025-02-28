0569f5bceb1de942b45ea5706222b7349cad6a39
Anthony de Melo <anthonymelo377@gmail.com>
2025-02-27 23:30:36 -0300

    Update d026.py

--- Desafios/d026.py
+++ Desafios/d026.py
@@ -2,3 +2,16 @@
 #Quantas vezes aparece a letra 'A'
 #Em que posição ela aparece a primeira vez
 #Em que posição ela aparece a última vez
+
+print('Desafio 26')
+
+frase = str(input('Digite uma frase: ')).strip()
+
+frase = frase.lower()
+ 
+print(f'A frase contém {frase.count('a')} letra(s) A ')
+
+print(f'{frase.find('a') + 1} é a posição da string que ela aparece pela primeira vez')
+
+print(f'{frase.rfind('a') + 1} é a posição da string que ela aparece pela última vez') #rfind significa que se irá procurar a letra 'a' a partir da direita
+
