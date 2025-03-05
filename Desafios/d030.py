01e9489c56eff84d167b306db33624dc1cef0876
Anthony de Melo <anthonymelo377@gmail.com>
2025-03-05 13:38:02 -0300

    Exericicios de python

---
+++ Desafios/d030.py
@@ -0,0 +1,13 @@
+#Exercicio de par ou impar
+
+print('Desafio 30')
+
+num = int(input('Informe um número: '))
+
+if (num % 2) == 0:
+    print('O número é par! ')
+
+else: 
+    print('O número é ímpar! ')
+    
+     
