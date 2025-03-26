ec168edd4c45988ce501987451f04fb7924c07c8
Anthony de Melo <anthonymelo377@gmail.com>
2025-03-26 19:54:38 -0300

    Create d039.py

---
+++ Desafios/d039.py
@@ -0,0 +1,11 @@
+#Faça~um programa que leia o ano de nascimento de um jovem e informe de acordo com sua idade:
+# Se ele ainda vai se alistar no serviço militar
+# Se é a hora de se alistar
+# Se já passou do tempo do alistamento
+# Seu programa também deverá mostrar o tempo que falta ou o prazo que passou do prazo
+
+import datetime
+
+print('Desafio 39')
+
+idade = str(input('Informe sua data de nascimento: '))
