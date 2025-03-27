a3c467597482c1394fbec0ce5c17c1d111724082
Anthony de Melo <anthonymelo377@gmail.com>
2025-03-26 23:19:52 -0300

    exercicios de python

--- Desafios/d037.py
+++ Desafios/d037.py
@@ -7,6 +7,15 @@
 print('Desafio 36')
 
 numero = int(input('Informe um número inteiro: '))
+base = int(input('Base de conversão: 1- Binario | 2- Octal | 3- Hexadecimal \n'))
 
+if base == 1:
+    print(f'O número convertido é: {bin(numero)}')
+
+elif base == 2:
+    print(f'O número convertido é: {oct(numero)}')
+
+else:
+    print(f'O número convertido é: {hex(numero)}')
 
 
--- Desafios/d039.py
+++ Desafios/d039.py
@@ -1,4 +1,4 @@
-#Faça~um programa que leia o ano de nascimento de um jovem e informe de acordo com sua idade:
+#Faça um programa que leia o ano de nascimento de um jovem e informe de acordo com sua idade:
 # Se ele ainda vai se alistar no serviço militar
 # Se é a hora de se alistar
 # Se já passou do tempo do alistamento
