731b6b327c3b0e33a2a7c75591d53660eac2b2fe
Anthony de Melo <anthonymelo377@gmail.com>
2025-04-03 21:45:13 -0300

    Update d038.py

--- Desafios/d038.py
+++ Desafios/d038.py
@@ -9,11 +9,11 @@ n1 = int(input('Informe um número: '))
 n2 = int(input('Informe outro número: '))
 
 if n1 > n2:
-    print('O primeiro valor é maior')
+    print('O primeiro valor é maior!')
 
 elif n2 > n1:
-    print('O segundo valor é maior')
+    print('O segundo valor é maior!')
 
 else: 
-    print('Os valores são iguais')
+    print('Os valores são iguais!')
  
